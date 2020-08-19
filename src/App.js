7d1c8ff9da951929dadcc6b46961294e571148f0
GabrielDeml <gabrielddeml@gmail.com>
2020-08-18 20:13:00 -0400

    Updated stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Navbar, Nav } from "react-bootstrap";
+import gitHubLogo from "./assets/GitHub-Mark-Light-64px.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyTest from "./Test";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Gabe Deml
-        </h1>
-      </header>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>Gabe Deml</Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link href="#About">About</Nav.Link>
+          <Nav.Link href="#Projects">Projects</Nav.Link>
+          <a href="https://github.com/GabrielDeml">
+            <img src={gitHubLogo} alt="GitHub" width="40" height="40" />
+          </a>
+        </Nav>
+      </Navbar>
+      <MyTest />
+    </>
   );
 }
 
---
+++ src/Test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+function MyTest() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#343434",
+      }}
+    >
+      <h1>Hello from test.js</h1>
+    </div>
+  );
+}
+
+export default MyTest;
